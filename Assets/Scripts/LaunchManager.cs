1cc7ff139f558c6e43862465759542868316a677
Resul <garnnet_221@hotmail.com>
2022-03-10 14:18:25 +0300

    Connection status UI and Lobby UI prepared.

--- Assets/Scripts/LaunchManager.cs
+++ Assets/Scripts/LaunchManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Photon.Pun;
@@ -5,21 +6,63 @@ using UnityEngine;
 
 public class LaunchManager : MonoBehaviourPunCallbacks
 {
+    [SerializeField] private GameObject _enterGamePanel;
+    [SerializeField] private GameObject _connectionStatusPanel;
+    [SerializeField] private GameObject _lobbyPanel;
+
+    #region Unity Methods
+
+    private void Start()
+    {
+        _enterGamePanel.SetActive(true);
+        _connectionStatusPanel.SetActive(false);
+        _lobbyPanel.SetActive(false);
+    }
+
+    #endregion
+    
+    #region Public Methods
+
     public void ConnectToPhotonServer()
     {
-        if(PhotonNetwork.IsConnected)
+        if (PhotonNetwork.IsConnected)
+        {
+            Debug.LogError("You already connected to the photon server!");
             return;
-        
+        }
+
+        if (PhotonNetwork.NickName.Length == 0)
+        {
+            Debug.LogError("NickName can not be empty!!!");
+            return;
+        }
+
+
         PhotonNetwork.ConnectUsingSettings();
+
+        _connectionStatusPanel.SetActive(true);
+        _enterGamePanel.SetActive(false);
     }
+
+    #endregion
+    
+    
+
+    #region Photon CallBacks
 
     public override void OnConnectedToMaster()
     {
         Debug.LogFormat($"{PhotonNetwork.NickName} Connected to Photon Server!");
+
+        _connectionStatusPanel.SetActive(false);
+        _lobbyPanel.SetActive(true);
     }
 
     public override void OnConnected()
     {
         Debug.Log("Connected to Internet!");
     }
+
+    #endregion
+    
 }
